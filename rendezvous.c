e0b2e13e598c483c6040a7d985154ab38f6a681c
rgrenger <35977310+rgrenger@users.noreply.github.com>
2020-04-13 13:19:17 -0600

    Update rendezvous.c

--- rendezvous.c
+++ rendezvous.c
@@ -8,13 +8,13 @@
 // before either prints their "after" message. Test by adding sleep(1)
 // calls in various locations.
 
-sem_t * s1, * s2;
+sem_t *s1, *s2;
 
 void *child_1(void *arg) {
     printf("child 1: before\n");
     // what goes here?
-    Sem_post(s1);
-    Sem_wait(s2);
+    sem_post(s1);
+    sem_wait(s2);
     printf("child 1: after\n");
     return NULL;
 }
@@ -22,8 +22,8 @@ void *child_1(void *arg) {
 void *child_2(void *arg) {
     printf("child 2: before\n");
     // what goes here?
-    Sem_post(s2);
-    Sem_wait(s1);
+    sem_post(s2);
+    sem_wait(s1);
     printf("child 2: after\n");
     return NULL;
 }
@@ -34,16 +34,16 @@ int main(int argc, char *argv[]) {
     // init semaphores here
     char * aArrived = "/aArrived";
     char * bArrived = "/bArrived";
-    s1 = Sem_open(aArrived, 0);
-    s2 = Sem_open(bArrived, 0);
+    s1 = sem_open(aArrived, 0);
+    s2 = sem_open(bArrived, 0);
     Pthread_create(&p1, NULL, child_1, NULL);
     Pthread_create(&p2, NULL, child_2, NULL);
     Pthread_join(p1, NULL);
     Pthread_join(p2, NULL);
-    Sem_close(s1);
-    Sem_close(s2);
-    Sem_unlink(aArrived);
-    Sem_unlink(bArrived);
+    sem_close(s1);
+    sem_close(s2);
+    sem_unlink(aArrived);
+    sem_unlink(bArrived);
     printf("parent: end\n");
     return 0;
 }
